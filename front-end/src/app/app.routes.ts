7f74be01bc0d29ec569826938cfa561f580568ea
Admir Jareb <admir.jareb@edu.fit.ba>
2023-12-25 21:44:30 +0100

    Generate UI for login
    Task 33

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {ProdavnicaComponent} from "../prodavnica/prodavnica.component";
 import {ProizvodViewComponent} from "../proizvod-view/proizvod-view.component";
 import {ObavijestiComponent} from "./obavijesti/obavijesti.component";
+import {PrijaviSeComponent} from "./prijavi-se/prijavi-se.component";
 
 export const routes: Routes = [
   { path: 'prodavnica', component: ProdavnicaComponent },
   { path: 'proizvod/:id', component: ProizvodViewComponent },
-  {path:'obavijesti', component: ObavijestiComponent}
+  {path:'obavijesti', component: ObavijestiComponent},
+  {path:'prijavi-se',component: PrijaviSeComponent}
 ];
---
+++ front-end/src/app/prijavi-se/prijavi-se.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-prijavi-se',
+  standalone: true,
+  imports: [],
+  templateUrl: './prijavi-se.component.html',
+  styleUrl: './prijavi-se.component.css'
+})
+export class PrijaviSeComponent implements OnInit{
+  ngOnInit(): void {
+  }
+
+}
