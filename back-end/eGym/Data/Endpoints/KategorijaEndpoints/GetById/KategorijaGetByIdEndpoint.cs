c06af79226cf0a26875755273f155461d59fa76e
Irfankcss <114685092+Irfankcss@users.noreply.github.com>
2024-08-04 17:01:32 +0200

    GetKategorijaById endpoint added

---
+++ back-end/eGym/Data/Endpoints/KategorijaEndpoints/GetById/KategorijaGetByIdEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using eGym.Data.Endpoints.KategorijaEndpoints.GetAll;
+using eGym.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eGym.Data.Endpoints.KategorijaEndpoints.GetById
+{
+    [Route("Kategorija")]
+    [Tags("Kategorija")]
+    public class KategorijaGetByIdEndpoint : MyBaseEndpoint<int, KategorijaGetResponse>
+    {
+        private readonly ApplicationDbContext _context;
+        public KategorijaGetByIdEndpoint(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Pretraga po Id")]
+        public override async Task<KategorijaGetResponse> Obradi([FromQuery] int Id, CancellationToken cancellationToken)
+        {
+            var kategorije = await _context.Kategorija.Where(x => x.Id == Id)
+                .Select(x => new KategorijaGetResponseKategorija
+                {
+                    KategorijaId = x.Id,
+                    Naziv = x.NazivKategorije,
+                    Opis = x.Opis,
+                }).ToListAsync(cancellationToken);
+
+            return new KategorijaGetResponse
+            {
+                Kategorije = kategorije,
+            };
+
+
+        }
+    }
+}
---
+++ back-end/eGym/Data/Endpoints/KategorijaEndpoints/GetById/KategorijaGetByIdResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace eGym.Data.Endpoints.KategorijaEndpoints.GetAll
+{
+    public class KategorijaGetByIdResponse
+    {
+        public List<KategorijaGetResponseKategorija> Kategorije {  get; set; }
+    }
+    public class KategorijaGetByIdResponseKategorija
+    {
+        public int KategorijaId {get;set;}
+        public string Naziv { get;set;}
+        public string Opis { get;set;}
+    }
+}
